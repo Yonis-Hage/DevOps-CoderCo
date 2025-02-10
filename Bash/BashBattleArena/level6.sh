0619850697723d11c7ad32a82f77f0146727099f
Yonis-Hage <crownpower721@gmail.com>
2025-02-10 17:56:01 +0000

    Update level6.sh

--- Bash/BashBattleArena/level6.sh
+++ Bash/BashBattleArena/level6.sh
@@ -3,15 +3,14 @@
 #I was very confused as to how to start this level. I researched and found that if you -z “$1” will show if an argument has been passed, $1 is the argument being passed and -z is checking for the empty string.
 
 # Checks if no file was provided as an argument
-if [ -z “$1” ] 
-then 
-echo “no file provided”
-Exit 1 # Exit the script with an error code
+if [ -z "$1" ]; then
+  echo "No file provided"
+  exit 1 # Exit the script with an error code
 fi  
 
 # Count the line numbers.
-LINE_COUNT=$(wc -l “$1” )
-echo “ The file “$1” has $LINE_COUNT amount of lines”
+LINE_COUNT=$(wc -l "$1")
+echo "The file '$1' has $LINE_COUNT lines"
 
 #The Line count is the variable, wc -l filename is used to count the lines in a file then include that with the $1 and you get the line count and echo it to find out the amount of lines in the terminal.
 
