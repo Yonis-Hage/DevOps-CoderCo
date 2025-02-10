3003b229570b9ea95f2cf525eb0311f275ded97b
Yonis-Hage <crownpower721@gmail.com>
2025-02-10 19:05:09 +0000

    Update level14.sh

--- Bash/BashBattleArena/level14.sh
+++ Bash/BashBattleArena/level14.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+echo "Select an option:"
+echo "1. Check Disk Space"
+echo "2. Show System Uptime"
+echo "3. List Users"
+echo "4. Exit"
+
+read -p "Enter your choice (1-4): " choice
+
+case $choice in
+  1) df -h 
+  ;;
+  2) uptime 
+  ;;
+  3) who 
+  ;;
+  4) echo "Exiting... Goodbye!" 
+  exit 0 
+  ;;
+  *) echo "Invalid option! Please enter a number between 1 and 4" 
+  ;;
+esac
