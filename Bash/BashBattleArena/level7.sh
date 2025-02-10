e92cd0e8c8e53c1a732b2cad6d8f577e7698f562
Yonis-Hage <crownpower721@gmail.com>
2025-02-10 17:50:52 +0000

    Update level7.sh

--- Bash/BashBattleArena/level7.sh
+++ Bash/BashBattleArena/level7.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+Directory= “Arena”
+
+# ! is a negation meaning not, combining that with -d means that it checks if the directory does not exist
+if [ ! -d “$DIRECTORY” ]; then
+echo “ Directory doesn’t exist”
+exit 1
+
+find “$directory” -type f -name “*.txt” -exec ls -lS {} +| awk ‘{print $5. $9} “ | sort -n
+
+#For the last line,  finds all txt files in the directory and filters only txt files, -type f ensure only files and not directories are considered. The ls -lS lists details on found files and sorts their size by largest to smallest. The ‘awk ‘{print $5. $9}’  extracts file size and filenames and the sort -n sorts the file numerically from smallest to largest.
 
