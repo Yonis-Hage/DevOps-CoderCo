77d737f66d1140f141a08d15cc657023fc27b521
Yonis-Hage <crownpower721@gmail.com>
2025-02-12 17:44:07 +0000

    Create FileOperation.sh

---
+++ Modules/Bash/Assignments/FileOperation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a directory named "FileOperation"
+mkdir -p FileOperation
+
+# Navigate into the directory
+cd FileOperation
+
+# Create a file named "MyFile.txt"
+touch FileOp.txt
+
+# Write some text into the file
+echo "Hello, I used a script to write the text into the file" > FileOp.txt
+
+# Display the contents of the file
+cat FileOp.txt
