6236eccf864f33ce0308d9f92c7ace572cf16a54
Yonis Hage <crownpower721@gmail.com>
2025-02-16 18:40:06 +0000

    The condition checking becomes more efficient and the code is more minimalistic, helps for cleaner code

--- Modules/Bash/Assignments/Conditional.sh
+++ Modules/Bash/Assignments/Conditional.sh
@@ -4,29 +4,13 @@
 read -p "Enter the filename to check: " filename
 
 # Check if the file exists
-if [ -e "$filename" ]; then
+if [[ -e "$filename" ]]; then
     echo "The file '$filename' exists."
 
-    # Check if the file is readable
-    if [ -r "$filename" ]; then
-        echo "The file is readable."
-    else
-        echo "The file is not readable."
-    fi
-
-    # Check if the file is writable
-    if [ -w "$filename" ]; then
-        echo "The file is writable."
-    else
-        echo "The file is not writable."
-    fi
-
-    # Check if the file is executable
-    if [ -x "$filename" ]; then
-        echo "The file is executable."
-    else
-        echo "The file is not executable."
-    fi
+    # Checks file permissions
+    [[ -r "$filename" ]] && echo "The file is readable." || echo "The file is not readable."
+    [[ -w "$filename" ]] && echo "The file is writable." || echo "The file is not writable."
+    [[ -x "$filename" ]] && echo "The file is executable." || echo "The file is not executable."
 
 else
     echo "The file '$filename' does not exist."
